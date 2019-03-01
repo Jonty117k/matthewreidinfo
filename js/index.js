1c2d52fbea839b60d216a264cf571434adc89315
Jonty117k <47775113+Jonty117k@users.noreply.github.com>
2019-02-28 21:35:17 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// $('#name-tag').hide();
+
+console.log("oh hi marc");
+// $( window ).on( "load", function() {
+	// $('#name-animation').show();
+//  });
+
+// setInterval(function(){
+// 		$('#name-tag').show();
+// }, 2000);
